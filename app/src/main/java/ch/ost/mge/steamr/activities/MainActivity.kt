5806a08c7b8553203fa155dc5e66caad00874037
Marcel Joss <marcel.joss@ost.ch>
2021-10-13 20:40:56 +0200

    feat(settings): add theme option

--- app/src/main/java/ch/ost/mge/steamr/activities/MainActivity.kt
+++ app/src/main/java/ch/ost/mge/steamr/activities/MainActivity.kt
@@ -1,16 +1,25 @@
-package ch.ost.mge.steamr
+package ch.ost.mge.steamr.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
 import ch.ost.mge.steamr.databinding.ActivityMainBinding
+import ch.ost.mge.steamr.util.applyTheme
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        applyTheme(PreferenceManager.getDefaultSharedPreferences(this))
+
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
+
+        binding.buttonNavigate.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
     }
 }
